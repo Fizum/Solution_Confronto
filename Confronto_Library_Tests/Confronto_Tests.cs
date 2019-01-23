206c55e5bf900c15d879dbbbef5b8895cdb779ca
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-01-23 09:13:48 +0100

    Programma che confronta e trova il massimo tra 2 numeri

---
+++ Confronto_Library_Tests/Confronto_Tests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Confronto_Library;
+
+namespace Confronto_Library_Tests
+{
+    [TestClass]
+    public class Confronto_Tests
+    {
+        [TestMethod]
+        public void TestMax()
+        {
+            int n1 = 0;
+            int n2 = 7;
+            int max_aspetto = 7;
+
+            int max_calcolato = Confronto_Library.Confronto.Max(n1, n2);
+            Assert.AreEqual(max_aspetto, max_calcolato);
+        }
+
+        public void TestMax2()
+        {
+            int n1 = -5;
+            int n2 = 10;
+            int max_aspetto = 10;
+
+            int max_calcolato = Confronto_Library.Confronto.Max(n1, n2);
+            Assert.AreEqual(max_aspetto, max_calcolato);
+        }
+    }
+}
---
+++ Confronto_Library_Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Confronto_Library_Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Confronto_Library_Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1795c3de-30af-474e-a832-16f956e85a29")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
